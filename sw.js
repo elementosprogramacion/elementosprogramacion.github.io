486bcd643495e14f24bd8006766235ee201dc359
Pablo <ingpvera@gmail.com>
2019-07-17 13:49:59 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'ep2019-170719';
+const resourcesToCache = [
+  'index.html' , 'unidades.html' , 'secuencial_ejercicios.html' , 'init.js' , 'estilos.css' , 'estilos.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(resourcesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
