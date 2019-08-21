f5f84654ff80c075c2d75c685baf4111fc17d4af
elementosprogramacion <52825449+elementosprogramacion@users.noreply.github.com>
2019-08-21 14:37:04 -0300

    Se agrega actualizacion al service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'ep-190719-1129';
+const cacheName = 'epUNLaM-v1';
 const resourcesToCache = [
   'index.html',
   'unidades.html',
@@ -13,6 +13,23 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+self.addEventListener('activate', function(event) {
+  var version = 'v1';
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames
+            .map(c => c.split('-'))
+            .filter(c => c[0] === 'epUNLaM')
+            .filter(c => c[1] !== version)
+            .map(c => caches.delete(c.join('-')))
+        )
+      )
+  );
+
+
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
