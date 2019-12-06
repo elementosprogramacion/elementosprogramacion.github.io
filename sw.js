c14cbb259517142645a1eb92bf1b06d383a9f56d
Pablo <ingpvera@gmail.com>
2019-12-06 10:09:28 -0300

    ajustes finales

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
-const cacheActual = 'epUNLaM-v10';
+const cacheActual = 'epUNLaM-v1';
 
 
 const paginasModificadas = [
-  'oldindex.html',
-  'css/estilos.css'
-    
+  'oldindex.html'
 ];
 
 const recursosACopiar = [
+  'css/estilos.css',
   'css/materialize.min.css',
   'js/materialize.min.js',
   'icons/apoyo.svg',
@@ -31,7 +30,46 @@ const recursosACopiar = [
   'icons/videos.svg',
   'icons/volver.svg',
   'icons/menu.svg',
-  'favicon.ico'  
+  'favicon.ico' ,
+  'adicionales.html' ,
+  'catedra.html' ,
+  'herramientas.html' ,
+  'index.html' ,
+  'iteracionCondicionada.html' ,
+  'menu.html' ,
+  'programados.html' ,
+  'u10_ad_archivos.html' ,
+  'u10_archivos.html' ,
+  'u10_ej_archivos.html' ,
+  'u10_vi_archivos.html' ,
+  'u11_ad_corte_de_control.html' ,
+  'u11_corte_de_control.html' ,
+  'u11_ej_corte_de_control.html' ,
+  'u1_introduccion.html' ,
+  'u1_vi_introduccion.html' ,
+  'u2_ad_secuencial.html' ,
+  'u2_ej_secuencial.html' ,
+  'u2_secuencial.html' ,
+  'u3_ad_lenguajeC.html' ,
+  'u3_lenguajeC.html' ,
+  'u4_decision.html' ,
+  'u4_ej_decision.html' ,
+  'u5.1_ej_iteracionDefinida.html' ,
+  'u5.2_ej_iteracionCondicionada.html' ,
+  'u5_iteracion.html' ,
+  'u6_ej_funciones.html' ,
+  'u6_funciones.html' ,
+  'u7.1_ej_vectores.html' ,
+  'u7.2_ej_matrices.html' ,
+  'u7_ad_arrays.html' ,
+  'u7_arrays.html' ,
+  'u7_vi_arrays.html' ,
+  'u8_ej_strings.html' ,
+  'u8_strings.html' ,
+  'u8_vi_strings.html' ,
+  'u9_ej_estructuras.html' ,
+  'u9_estructuras.html' ,
+  'unidades.html'
 ];
 
 
